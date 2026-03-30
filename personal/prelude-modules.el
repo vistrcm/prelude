9d32e38751f2e43402f77759ee2e61c3ce770fb0
Stanislav Vitko <stas@vitko.me>
2026-03-30 16:27:18 -0700

    disable prelude-lsp

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -79,7 +79,7 @@
 (require 'prelude-js)
 ;; (require 'prelude-latex)
 (require 'prelude-lisp) ;; Common setup for Lisp-like languages
-(require 'prelude-lsp) ;; Base setup for the Language Server Protocol
+;; (require 'prelude-lsp) ;; Base setup for the Language Server Protocol
 ;; (require 'prelude-lua)
 ;; (require 'prelude-ocaml)
 (require 'prelude-perl)
